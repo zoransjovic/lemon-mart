820f3f3f149426fa9e950e418d91f627258cd8a1
Zoran Jovic <zoransjovic@gmail.com>
2019-08-02 14:57:54 +0200

    Added parentheses

--- src/app/common/simple-dialog/simple-dialog.component.ts
+++ src/app/common/simple-dialog/simple-dialog.component.ts
@@ -3,13 +3,15 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material'
 
 @Component({
   template: `
-    <h2 mat-dialog-title>data.title</h2>
+    <h2 mat-dialog-title>{{ data.title }}</h2>
     <mat-dialog-content>
-      <p>data.content</p>
+      <p>{{ data.content }}</p>
     </mat-dialog-content>
     <mat-dialog-actions>
       <span class="flex-spacer"></span>
-      <button mat-button mat-dialog-close *ngIf="data.cancelText">data.cancelText</button>
+      <button mat-button mat-dialog-close *ngIf="data.cancelText">
+        {{ data.cancelText }}
+      </button>
       <button
         mat-button
         mat-button-raised
@@ -17,7 +19,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material'
         [mat-dialog-close]="true"
         cdkFocusInitial
       >
-        data.okText
+        {{ data.okText }}
       </button>
     </mat-dialog-actions>
   `,
